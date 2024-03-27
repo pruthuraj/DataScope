43be42701a71987739e11166157b8ef2928ce1fc
pruthu <pruthuparikh@gmail.com>
2024-03-27 18:58:22 +0530

    create user done

--- DataScope/urls.py
+++ DataScope/urls.py
@@ -2,12 +2,16 @@ from django.contrib import admin
 from django.urls import path,include
 from pages import views
 from user import views as userV
+
 urlpatterns = [
     path('',views.index,name='home'),
     path('admin/', admin.site.urls),
     path('login/',views.login),
     path('signup/',views.signup),
+    path('createuser',views.createUser,name='createuser'),
     path('about/',views.about),
     path('contact/',views.contact),
     path('home/',userV.home),
+    path('home/mine',userV.mine),
+    path('home/minedata',userV.mindata,name='btnmine'),
 ]
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # DataScope
 A web Scrapping web-application
+ required 
+
+ mongodb database
+ django
+
+ extra library
+ pip install pymongo
